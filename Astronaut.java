b918443cb50b2fcccf7a790a589a7f5aef6ad282
Arthur Azoula <arthur.azoula@epitech.eu>
2023-10-25 10:39:27 +0200

    Add exercice 04

--- Astronaut.java
+++ Astronaut.java
@@ -31,4 +31,26 @@ public class Astronaut {
         return this.destination;
     }
 
+    public void doActions(Object mars) {
+        if(mars instanceof planet.Mars) {
+            // cast mars to planet.Mars
+            planet.Mars planetMars = (planet.Mars) mars;
+            this.destination = planetMars.getLandingSite();
+            System.out.println(this.name + ": Started a mission!");
+        }
+        else if (mars instanceof chocolate.Mars) {
+            // cast mars to chocolate.Mars
+            chocolate.Mars chocolateMars = (chocolate.Mars) mars;
+            this.snacks++;
+            System.out.println(this.name + ": Thanks for this Mars number " + chocolateMars.getId());
+            System.out.println(this.name + ": I may have done nothing, but I have " + this.snacks + " Mars to eat at least!");
+
+        }
+    }
+
+    public void doActions() {
+        System.out.println(this.name + ": Nothing to do.");
+        System.out.println(this.name + ": I may have done nothing, but I have " + this.snacks + " Mars to eat at least!");
+    }
+
 }
